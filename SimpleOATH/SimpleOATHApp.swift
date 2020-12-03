5f465805c162cfa5be1b53223f4c1eecfefdfc29
Jens Utbult <jens.utbult@yubico.com>
2020-12-03 20:00:32 +0100

    Initial Commit

---
+++ SimpleOATH/SimpleOATHApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleOATHApp.swift
+//  SimpleOATH
+//
+//  Created by Jens Utbult on 2020-12-03.
+//
+
+import SwiftUI
+
+@main
+struct SimpleOATHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
